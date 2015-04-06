8081994aff9ca9b56e39d375fe0aa447bb808a54
Joern Bernhardt <jb@campudus.com>
2015-04-06 17:02:09 +0200

    Add hook for ant.properties and sign apk
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ hooks/before_build/add_ant_properties.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var rootDir = process.argv[2];
+var platforms = process.env['CORDOVA_PLATFORMS'].split(',');
+var config = require(rootDir + '/config.json');
+var antFile;
+
+if (platforms.indexOf('android') !== -1) {
+  antFile = rootDir + '/platforms/android/ant.properties';
+  if (config.key) {
+    console.log('android platform exists, adding ant.properties');
+    props = 'key.store=' + config.key.store;
+    props += '\nkey.store.password=' + config.key.storePassword;
+    props += '\nkey.alias=' + config.key.alias;
+    props += '\nkey.alias.password=' + config.key.aliasPassword;
+    fs.writeFileSync(antFile, props, 'utf8');
+  } else {
+    console.log('config.key not set, adding empty ant.properties');
+    fs.writeFileSync(antFile, '', 'utf8');
+  }
+} else {
+  console.log('android platform does not exist, NOT adding ant.properties');
+}
