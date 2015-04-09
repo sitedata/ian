0fd462eb1e8c9f388f5a458416e57805111b434d
Joern Bernhardt <jb@campudus.com>
2015-04-09 07:55:15 +0200

    Try out jquery.fittext
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,26 @@
 var settings = require('./settings.js');
-var namegen = require('./namegen.js');
+var nameGen = require('./namegen.js');
 var names = require('./texts.json');
 var $ = require('jQuery');
+require('jQuery-fittext');
 var attachFastClick = require('fastclick');
-
 var $text = $('#generated').find('.text');
 
 attachFastClick(document.body);
 
-$('#generated').click(pickAndChange);
+var $generated = $('#generated');
+$generated.fitText(1.5, {
+  minFontSize: 15,
+  maxFontSize: 40
+});
+
+$generated.click(pickAndChange);
 pickAndChange();
 
 
 function pick() {
   var lang = names[Math.floor(Math.random() * names.length)];
-  return namegen.fromArray(lang);
+  return nameGen.fromArray(lang);
 }
 
 function pickAndChange() {
