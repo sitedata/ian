3130bdececaee5b2c6dfd14c69404a70ce01ad47
Joern Bernhardt <jb@campudus.com>
2015-04-11 21:20:13 +0200

    Do not quit on replacement error - file might not exist
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- hooks/after_prepare/lib/replacer.js
+++ hooks/after_prepare/lib/replacer.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
 function replacer(fileName, opts) {
-  var data = fs.readFileSync(fileName, 'utf8');
-  opts.forEach(function(elem) {
-    data = data.replace(elem.regex, elem.substitution);
-  });
-  fs.writeFileSync(fileName, data);
+  try {
+    var data = fs.readFileSync(fileName, 'utf8');
+    opts.forEach(function (elem) {
+      data = data.replace(elem.regex, elem.substitution);
+    });
+    fs.writeFileSync(fileName, data);
+  } catch (ex) {
+    console.log('could not read / write file ' + fileName);
+    console.log(ex);
+  }
 }
 
 module.exports = replacer;
