95df23258f2114ce1ce979fd5231a40ddba8e8e7
Joern Bernhardt <jb@campudus.com>
2015-04-11 22:19:19 +0200

    Move ownership to campudus, add all texts
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- hooks/before_build/add_ant_properties.js
+++ hooks/before_build/add_ant_properties.js
@@ -10,10 +10,18 @@ if (platforms.indexOf('android') !== -1) {
   antFile = rootDir + '/platforms/android/ant.properties';
   if (config.key) {
     console.log('android platform exists, adding ant.properties');
-    props = 'key.store=' + config.key.store;
-    props += '\nkey.store.password=' + config.key.storePassword;
-    props += '\nkey.alias=' + config.key.alias;
-    props += '\nkey.alias.password=' + config.key.aliasPassword;
+    if (config.key.store) {
+      props = 'key.store=' + config.key.store;
+    }
+    if (config.key.storePassword) {
+      props += '\nkey.store.password=' + config.key.storePassword;
+    }
+    if (config.key.alias) {
+      props += '\nkey.alias=' + config.key.alias;
+    }
+    if (config.key.aliasPassword) {
+      props += '\nkey.alias.password=' + config.key.aliasPassword;
+    }
     fs.writeFileSync(antFile, props, 'utf8');
   } else {
     console.log('config.key not set, adding empty ant.properties');
